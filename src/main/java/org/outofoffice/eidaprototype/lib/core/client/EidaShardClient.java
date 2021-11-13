4dda5fbf9f783c5e2234313d272e496d216937db
josh <suloginscene@gmail.com>
2021-11-13 15:37:04 +0900

    [#8] feat: dml 제너레이터 구현

--- src/main/java/org/outofoffice/eidaprototype/lib/core/client/EidaShardClient.java
+++ src/main/java/org/outofoffice/eidaprototype/lib/core/client/EidaShardClient.java
@@ -20,13 +20,13 @@ public class EidaShardClient implements EidaDmlClient, EidaDdlShardClient {
 
     @Override
     public <ID> String selectById(String shardUrl, String tableName, ID id) {
-        String dml = dmlGenerator.createSelectByIdQuery(shardUrl, tableName, id);
+        String dml = dmlGenerator.createSelectByIdQuery(tableName, id);
         return eidaClient.request(shardUrl, dml);
     }
 
     @Override
     public void insert(String shardUrl, String tableName, String serialized) {
-        String dml = dmlGenerator.createInsertQuery(shardUrl, tableName, serialized);
+        String dml = dmlGenerator.createInsertQuery(tableName, serialized);
         eidaClient.request(shardUrl, dml);
     }
 
--- src/main/java/org/outofoffice/eidaprototype/lib/core/query/EidaDmlGenerator.java
+++ src/main/java/org/outofoffice/eidaprototype/lib/core/query/EidaDmlGenerator.java
@@ -4,15 +4,15 @@ package org.outofoffice.eidaprototype.lib.core.query;
 public class EidaDmlGenerator {
 
     public String createSelectAllQuery(String tableName) {
-        return "dml query";
+        return "select " + tableName;
     }
 
-    public <ID> String createSelectByIdQuery(String shardUrl, String tableName, ID id) {
-        return "dml query";
+    public <ID> String createSelectByIdQuery(String tableName, ID id) {
+        return "select " + tableName + " " + id;
     }
 
-    public String createInsertQuery(String shardUrl, String tableName, String serialized) {
-        return "dml query";
+    public String createInsertQuery(String tableName, String serialized) {
+        return "insert " + tableName + " " + serialized;
     }
 
 }
