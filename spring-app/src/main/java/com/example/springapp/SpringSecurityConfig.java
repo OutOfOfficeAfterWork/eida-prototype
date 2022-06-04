1e4bccf8d2db3e77dd92a4503511059fef463d2e
eraserrr <>
2022-06-04 14:05:50 +0900

    feat: 스프링 웹 예제 초기 설정

---
+++ spring-app/src/main/java/com/example/springapp/SpringSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.example.springapp;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import static org.springframework.http.HttpMethod.GET;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .mvcMatchers(GET,
+                "/", "/hello"
+            ).permitAll()
+            .anyRequest().authenticated()
+        ;
+    }
+
+}
