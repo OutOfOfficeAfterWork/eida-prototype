6d216d18e4e1490e6985c0e7ddb03950c7d8c763
josh <suloginscene@gmail.com>
2022-06-12 17:11:43 +0900

    feat: 수강신청 내역 보기(관리자)

--- spring-app/src/main/java/com/example/springapp/controller/EnrollmentController.java
+++ spring-app/src/main/java/com/example/springapp/controller/EnrollmentController.java
@@ -1,17 +1,30 @@
 package com.example.springapp.controller;
 
 import com.example.springapp.application.EnrollmentService;
+import com.example.springapp.application.SubjectService;
+import com.example.springapp.domain.Enrollment;
+import com.example.springapp.domain.Subject;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
 public class EnrollmentController {
     private final EnrollmentService enrollmentService;
+    private final SubjectService subjectService;
 
     @GetMapping("/enrollment-list")
-    public String getEnrollmentList() {
+    public String getEnrollmentList(Model model) {
+        List<Subject> subjects = subjectService.findAll();
+        Map<Subject, List<Enrollment>> enrollmentMap = new HashMap<>();
+        subjects.forEach(subject -> enrollmentMap.put(subject, enrollmentService.findBySubject(subject.getSubjectName())));
+        model.addAttribute("enrollmentMap", enrollmentMap);
         return "enrollment-list";
     }
 }
