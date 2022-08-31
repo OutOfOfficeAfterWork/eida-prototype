7a6ac69312db8d6f1f935d9a139265e87eed8941
eraserrr <a01074761054@gmail.com>
2022-08-31 21:44:42 +0900

    feat: file facade, repository 삭제

--- eida-manager/src/main/java/org/outofoffice/eida/manager/repository/SchemeRepository.java
+++
@@ -1,9 +0,0 @@
-package org.outofoffice.eida.manager.repository;
-
-
-public interface SchemeRepository {
-    void save(String tableName, String scheme);
-    String findByName(String tableName);
-    void rename(String currentName, String nextName);
-    void delete(String tableName);
-}
--- eida-manager/src/main/java/org/outofoffice/eida/manager/repository/ShardMappingRepository.java
+++
@@ -1,9 +0,0 @@
-package org.outofoffice.eida.manager.repository;
-
-import org.outofoffice.eida.manager.domain.ShardMapping;
-
-public interface ShardMappingRepository {
-    ShardMapping find();
-
-    void save(ShardMapping shardMapping);
-}
